cf3370d7c7a9776701fce43d1d3eef1f01fd001c
Leonardo <leo.tmartinez@usp.br>
2017-10-25 14:38:21 -0200

    first commit

---
+++ src/pages/carlos/carlos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CarlosPage } from './carlos';
+
+@NgModule({
+  declarations: [
+    CarlosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CarlosPage),
+  ],
+})
+export class CarlosPageModule {}
